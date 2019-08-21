698b6be80a1174d8421f4b5bfdbd34c57b732e26
Patrick Marschik <p.marschik@ixolit.com>
2019-08-21 13:14:30 +0200

    moved repository

--- src/main/java/com/ixopay/client/signing/HttpHeaders.java
+++
@@ -1,8 +0,0 @@
-package com.ixopay.client.signing;
-
-public final class HttpHeaders {
-	private HttpHeaders() {}
-
-	public static final String AUTHORIZATION = "Authorization";
-	public static final String DATE = "Date";
-}
